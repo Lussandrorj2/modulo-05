333b48bdf8d3a74717db32b8162fcf0c2724c2e4
kauazin21 <167949109+kauazin21@users.noreply.github.com>
2025-12-15 15:24:50 -0300

    chore: alteração url,  taredas com logout

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import ListaTarefasAPIView
+from .views import LogoutView, TarefaListCreateAPIView, TarefaRetrieveUpdateDestroyAPIView
 
 app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
+    path('tarefas/<int:pk>/', TarefaRetrieveUpdateDestroyAPIView.as_view(), name='tarefa-detail'),
+    path('logout/', LogoutView.as_view(), name='logout'), # ← Novo endpoint
 ]
