841879ae18a532a40432ead9015c9117d37c7e00
kauazin21 <167949109+kauazin21@users.noreply.github.com>
2025-12-15 15:25:53 -0300

    update: alteração no views. Adicionado token

--- core/views.py
+++ core/views.py
@@ -5,7 +5,52 @@ from .models import Tarefa
 from .serializers import TarefaSerializer
 from django.db import IntegrityError
 import logging
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Tarefa
+from .serializers import TarefaSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
 logger = logging.getLogger(__name__)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            token = RefreshToken(refresh_token)
+            token.blacklist() # Adiciona o token à lista negra
+            
+            return Response(
+                {"detail": "Logout realizado com sucesso."},
+                status=status.HTTP_205_RESET_CONTENT # 205 é a resposta padrão para "reset content"
+            )
+        except Exception: # Captura exceções como token_not_valid
+            return Response(
+                {"detail":"Token inválido."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class TarefaListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
+    permission_classes = [IsAuthenticated] # ← Proteção
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+class TarefaRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
+    permission_classes = [IsAuthenticated] # ← Proteção
+    
+
 
 
 class ListaTarefasAPIView(APIView):
@@ -45,3 +90,9 @@ class ListaTarefasAPIView(APIView):
                 {'error': 'Erro interno do servidor.'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+class MinhaView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        print(f"Usuário autenticado: {request.user.username}")
