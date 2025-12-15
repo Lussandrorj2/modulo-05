008c12f0a0c7bd0b5344dd3e03d8fa71e6308cb3
kauazin21 <167949109+kauazin21@users.noreply.github.com>
2025-12-15 15:28:40 -0300

    update: urls adicionando token

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core.views import CustomTokenObtainPairView
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, # Login (obter access e refresh tokens)
     TokenRefreshView, # Renovar token
 )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +31,8 @@ urlpatterns = [
     path('api/token/refresh',
          TokenRefreshView.as_view(),
          name='token_refresh'),
+    path('api/token/',
+         CustomTokenObtainPairView.as_view(), 
+         name='token_obtain_pair'),
     path('api/', include('core.urls')),
 ]
